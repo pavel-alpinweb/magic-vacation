b6e4f4a63363b52fca0c0b17c5622528567554cf
Pavel Bezdornov <alpinweb@yandex.ru>
2025-03-03 10:57:07 +0600

    Изменение animateNumber

--- source/js/modules/animateNumber.js
+++ source/js/modules/animateNumber.js
@@ -1,10 +1,25 @@
 export default function (callback, max, step, fps) {
   let currentStep = 0;
   const frameTime = 1000 / fps;
+  let now;
+  let then = Date.now();
+  let elapsed;
 
   requestAnimationFrame(function draw() {
+    // проверяем, сколько времени прошло с предыдущего запуска
+    now = Date.now();
+    elapsed = now - then;
+    // вычисляем текущий шаг
     currentStep = (currentStep + step) < max ? currentStep + step : max;
-    callback(currentStep);
+
+    if (elapsed > frameTime) {
+      // сохранение времени текущей отрисовки кадра
+      then = now - (elapsed % frameTime);
+
+      // запуск функции отрисовки
+      callback(currentStep);
+    }
+
 
     if (currentStep < max) {
       setTimeout(() => {
